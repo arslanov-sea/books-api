f769092b7d0510d782ba7f644a01f3565d450f96
Suleiman <arslanov.sea@gmail.com>
2026-07-05 11:24:21 +0300

    fix. исправление синтаксических ошибок

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ async def get_public_books():
     return {"message": "all public books endpoint"}
 
 
-@app.get("/books/public/{book_id}"):
+@app.get("/books/public/{book_id}")
 async def get_public_book(book_id: int):
     return {"message": f"public book with id {book_id} endpoint"}
 
@@ -25,37 +25,37 @@ async def create_public_book():
     return {"message": "create public book endpoint"}
 
 
-@app.patch("/books/public/{book_id}"):
+@app.patch("/books/public/{book_id}")
 async def update_public_book(book_id: int):
     return {"message": f"update public book with id {book_id} endpoint"}
 
 
-@app.delete("/books/public/{book_id}"):
+@app.delete("/books/public/{book_id}")
 async def delete_public_book(book_id: int):
     return {"message": f"delete public book with id {book_id} endpoint"}
 
 
-@app.get(/authors):
+@app.get("/authors")
 async def get_authors():
     return {"message": "all authors endpoint"}
 
 
-@app.get("/authors/{author_id}"):
+@app.get("/authors/{author_id}")
 async def get_author(author_id: int):
     return {"message": f"author with id {author_id} endpoint"}
 
 
-@app.post("/authors"):
+@app.post("/authors")
 async def create_author():
     return {"message": "create author endpoint"}
 
 
-@app.patch("/authors/{author_id}"):
+@app.patch("/authors/{author_id}")
 async def update_author(author_id: int):
     return {"message": f"update author with id {author_id} endpoint"}
 
 
-@app.delete("/authors/{author_id}"):
+@app.delete("/authors/{author_id}")
 async def delete_author(author_id: int):
     return {"message": f"delete author with id {author_id} endpoint"}
 
@@ -65,21 +65,21 @@ async def get_genres():
     return {"message": "all genres endpoint"}
 
 
-@app.get("/genres/{genre_id}"):
+@app.get("/genres/{genre_id}")
 async def get_genre(genre_id: int):
     return {"message": f"genre with id {genre_id} endpoint"}
 
 
-@ap.post("/genres")
+@app.post("/genres")
 async def create_genre():
     return {"message": "create genre endpoint"}
 
 
-@app.patch("/genres/{genre_id}"):
+@app.patch("/genres/{genre_id}")
 async def update_genre(genre_id: int):
     return {"message": f"update genre with id {genre_id} endpoint"}
 
 
-@app.delete("/genres/{genre_id}"):
+@app.delete("/genres/{genre_id}")
 async def delete_genre(genre_id: int):
     return {"message": f"delete genre with id {genre_id} endpoint"}
