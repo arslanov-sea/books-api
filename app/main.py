3b4d73561adfdd6f398597727e94472d88594be4
Suleiman <arslanov.sea@gmail.com>
2026-07-17 16:14:52 +0300

    feat: добавить валидацию offset, limit, id значений

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel
+from typing import Annotated
 
 
 class Author(BaseModel):
@@ -30,14 +31,13 @@ app = FastAPI()
 async def root():
     return {"message": "root endpoint"}
 
-
 @app.get("/books/public")
-async def get_public_books(offset: int = 0, limit: int = 10):
+async def get_public_books(offset: Annotated[int, Query(ge=0, le=5000)] = 0, limit: Annotated[int, Query(ge=1, le=100)] = 10):
     return {"message": f"all public books endpoint. offset: {offset}, limit: {limit}"}
 
 
 @app.get("/books/public/{book_id}")
-async def get_public_book(book_id: int):
+async def get_public_book(book_id: Annotated[int, Path(ge=1)]):
     return {"message": f"public book with id {book_id} endpoint"}
 
 
@@ -47,22 +47,21 @@ async def create_public_book():
 
 
 @app.patch("/books/public/{book_id}")
-async def update_public_book(book_id: int):
+async def update_public_book(book_id: Annotated[int, Path(ge=1)]):
     return {"message": f"update public book with id {book_id} endpoint"}
 
 
 @app.delete("/books/public/{book_id}")
-async def delete_public_book(book_id: int):
+async def delete_public_book(book_id: Annotated[int, Path(ge=1)]):
     return {"message": f"delete public book with id {book_id} endpoint"}
 
-
 @app.get("/authors")
-async def get_authors(offset: int = 0, limit: int = 10):
+async def get_authors(offset: Annotated[int, Query(ge=0, le=5000)] = 0, limit: Annotated[int, Query(ge=1, le=100)] = 10):
     return {"message": f"all authors endpoint. offset: {offset}, limit: {limit}"}
 
 
 @app.get("/authors/{author_id}")
-async def get_author(author_id: int):
+async def get_author(author_id: Annotated[int, Path(ge=1)]):
     return {"message": f"author with id {author_id} endpoint"}
 
 
@@ -72,22 +71,21 @@ async def create_author():
 
 
 @app.patch("/authors/{author_id}")
-async def update_author(author_id: int):
+async def update_author(author_id: Annotated[int, Path(ge=1)]):
     return {"message": f"update author with id {author_id} endpoint"}
 
 
 @app.delete("/authors/{author_id}")
-async def delete_author(author_id: int):
+async def delete_author(author_id: Annotated[int, Path(ge=1)]):
     return {"message": f"delete author with id {author_id} endpoint"}
 
-
 @app.get("/genres")
-async def get_genres(offset: int = 0, limit: int = 10):
+async def get_genres(offset: Annotated[int, Query(ge=0, le=5000)] = 0, limit: Annotated[int, Query(ge=1, le=100)] = 10):
     return {"message": f"all genres endpoint. offset: {offset}, limit: {limit}"}
 
 
 @app.get("/genres/{genre_id}")
-async def get_genre(genre_id: int):
+async def get_genre(genre_id: Annotated[int, Path(ge=1)]):
     return {"message": f"genre with id {genre_id} endpoint"}
 
 
@@ -97,10 +95,10 @@ async def create_genre():
 
 
 @app.patch("/genres/{genre_id}")
-async def update_genre(genre_id: int):
+async def update_genre(genre_id: Annotated[int, Path(ge=1)]):
     return {"message": f"update genre with id {genre_id} endpoint"}
 
 
 @app.delete("/genres/{genre_id}")
-async def delete_genre(genre_id: int):
+async def delete_genre(genre_id: Annotated[int, Path(ge=1)]):
     return {"message": f"delete genre with id {genre_id} endpoint"}
